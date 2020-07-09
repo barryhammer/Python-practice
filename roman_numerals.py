bbbeb68eca8fa71c383778275c6cc487abd33741
barryhammer <65852423+barryhammer@users.noreply.github.com>
2020-07-09 10:19:46 +0300

    Roman numerals 1.1
    After spending WAY too much time trying to figure this out on my own, I throw in the towel and find code much more elegant than mine. I'm leaving my original code as part of my learning process.

--- roman_numerals.py
+++ roman_numerals.py
@@ -2,6 +2,13 @@ import ctypes
 
 my_input = input("Please enter a roman numeral: ")
 
+'''
+This was my attempt at converting Roman Numerals. There were two problems:
+    1) Although I could iter through the list, it kept adding the next value, even if it shouldn't have (EG. MMCM = 3100 instead of 2900)
+    2) I knew that my code should be much shorter for what this is.
+In the end, I realized I was spending way too much time trying to re-invent the wheel.
+This code I'm leaving here for my own use, as part of my learning process.
+
 # Split value into individual characters that can be iterated
 def split(word):
     return list(word)
@@ -55,6 +62,28 @@ for i in my_list:
                 my_num += 1
     else:
         my_num += 0
+'''
 
-ctypes.windll.user32.MessageBoxW(0, "Your Roman Numeral {} equals {}.".format(my_input, my_num), "Roman Numeral Converter", 1)
+'''
+This code I found at https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s24.html
+I just removed the exception errors, since I didn't think they were necessary for this exercize.
+'''
+nums = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+sum = 0
+for i in range(len(my_input)):
+        value = nums[my_input[i]]
+        # If the next place holds a larger number, this value is negative
+        if i+1 < len(my_input) and nums[my_input[i+1]] > value:
+            sum -= value
+        else: sum += value
+
+ctypes.windll.user32.MessageBoxW(0, "Your Roman Numeral {} equals {}.".format(my_input, sum), "Roman Numeral Converter", 1)
 # Found this at https://stackoverflow.com/questions/2963263/how-can-i-create-a-simple-message-box-in-python
+
+'''
+In the end, there were a few lessons learned:
+    1) If it's taking too long to find the solution, you need to get the help of someone else who can point you in the right direction
+    2) Although I did learn about dictionaries, I didn't think to use them. They make this solution much more elegant.
+    3) I was wracking my brains trying to get nums[my_input[i+1]] properly. Hopefully now that I have this solution at hand, I'll be able
+        to reference it again in the future.
+'''
